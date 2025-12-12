2dcaf5de582887b66a91a911f76ec981f9e5b7e6
alaaM <alaaMandour2024@gmail.com>
2025-12-12 14:12:36 +0200

    bonus Q

--- Assignment2/bonus.js
+++ Assignment2/bonus.js
@@ -10,3 +10,33 @@ Output: 9
 Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
 */
 
+function findKthPositive(arr,k){
+    let count = 0;
+
+    let max = arr[0];
+    for (let i = 0; i <arr.length; i++) {
+        if(arr[i]>max){
+            max = arr[i]
+        }
+    }
+
+    let nums = []
+    for (let i = 0; i < max + k ; i++) { // عملت max + K علشان لو الرقم المفقود كان بعد اخر رقم في ال arr
+        nums[i] = i+1;
+    }
+
+    let exist = false;
+    for (let i = 0; i < nums.length; i++) {
+        let exist = arr.includes(nums[i]);
+
+        if (!exist) {
+            count++;
+        }
+        if(count == k){
+            return nums[i]
+        }
+    }
+}
+
+// let arr = [1,2,3,4];
+// console.log(findKthPositive(arr , 2));
